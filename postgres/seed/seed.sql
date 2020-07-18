89636d249bb2bcc3d80b99c0ed1c8ab473e1f4a9
Ceonsamea <killer_22758@hotmail.com>
2020-07-18 09:20:09 +0800

    changes seed user info

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) values ('Test', 'test@gmail.com', 5, '2018-01-01');
+INSERT into users (name, email, entries, joined) values ('a', 'a@a.com', 5, '2018-01-01');
 INSERT into login (hash, email) values ('$2a$10$joqwWutyIxfucntraGDg8OujeyT3lIbro9PF9N6o6vXX.gxXKdaTu', 'test@gmail.com');
 
 
