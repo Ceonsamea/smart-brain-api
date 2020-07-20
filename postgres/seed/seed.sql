7e1ddced4951da477b4324f81a3e8f64f96a0542
Ceonsamea <killer_22758@hotmail.com>
2020-07-20 18:00:38 +0800

    added user profile on frontend

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,7 +1,8 @@
 BEGIN TRANSACTION;
 
 INSERT into users (name, email, entries, joined) values ('a', 'a@a.com', 5, '2018-01-01');
-INSERT into login (hash, email) values ('$2a$10$joqwWutyIxfucntraGDg8OujeyT3lIbro9PF9N6o6vXX.gxXKdaTu', 'test@gmail.com');
+-- pw is 123
+INSERT into login (hash, email) values ('$2a$10$WjIpIT81pIANJrO4lHEhveldTWsxNvnuMvKB9ln4Hftm8M.jE/eB2', 'a@a.com');
 
 
 COMMIT
